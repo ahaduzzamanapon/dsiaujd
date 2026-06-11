4bc7bc2c2ff5f422b2a1a3e4fb0b48d9144c47dd
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2026-06-11 10:37:52 +0600

    Some Changes From Apon

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->validateCsrfTokens(except: [
+            'api/devices/ping',
+        ]);
         $middleware->alias([
             'admin.auth' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
